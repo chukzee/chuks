c5201495afb636bc0e7e7749aafb3e5ab9a52dfc
Chuks Alimele <chuksalimele@gmail.com>
2017-08-06 01:51:35 +0100

    chuks

--- public/www/app/Main.js
+++ public/www/app/Main.js
@@ -56,6 +56,46 @@ var Main = {};
 
     };
 
+    /**
+     * Self invoking function for polyfill
+     * largely gotten from https://developer.mozilla.org
+     * @returns {undefined}
+     */
+    (function() {
+        
+        //polyfill for bind - MDN ->https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+        if (!Function.prototype.bind) {
+            Function.prototype.bind = function (oThis) {
+                if (typeof this !== 'function') {
+                    // closest thing possible to the ECMAScript 5
+                    // internal IsCallable function
+                    throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+                }
+
+                var aArgs = Array.prototype.slice.call(arguments, 1),
+                        fToBind = this,
+                        fNOP = function () {},
+                        fBound = function () {
+                            return fToBind.apply(this instanceof fNOP
+                                    ? this
+                                    : oThis,
+                                    aArgs.concat(Array.prototype.slice.call(arguments)));
+                        };
+
+                if (this.prototype) {
+                    // Function.prototype doesn't have a prototype property
+                    fNOP.prototype = this.prototype;
+                }
+                fBound.prototype = new fNOP();
+
+                return fBound;
+            };
+        }
+        
+        //more polyfill may go below 
+        
+    })();
+
     function xhrReq(send, method, url) {
         var data, successFn, errorFn, completeFn;
         if (arguments.length === 4) {
