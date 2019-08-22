653b6cae7cab48f9091b4fab35f07f2fd94c67b4
Chuks Alimele <chuksalimele@gmail.com>
2019-08-22 22:04:06 +0100

    chuks

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'chuksalimele@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Vehicle Ticketing Services'),
     ],
 
