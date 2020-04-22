fc99dc4a73c787a541ce98a3cfccc0f41d298a54
Chuks Alimele <chuksalimele@gmail.com>
2020-04-22 13:23:56 +0100

    Chuks commit

--- app/src/main/java/com/beepmemobile/www/MainActivity.kt
+++ app/src/main/java/com/beepmemobile/www/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.beepmemobile.www
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-        //This method sets the "main_activity_top_toolbar" toolbar as the app bar for the activity.
-        setSupportActionBar(findViewById(R.id.main_activity_top_toolbar))
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
-
 
 }
